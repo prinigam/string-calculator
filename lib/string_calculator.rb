b7f124477bd0b9228348db04375012e75e5c7daa
Pri-Nigam <priyanshunigam1607@gmail.com>
2025-09-12 15:51:36 +0530

    feat: handle comma separated numbers input

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ''
     return numbers.to_i if numbers.match?(/^\d+$/)
+
+    numbers.split(',').map(&:to_i).sum
   end
 end
