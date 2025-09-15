b0bd2211bece31abb8783a6c8c095754d7eb9f04
Pri-Nigam <priyanshunigam1607@gmail.com>
2025-09-15 12:51:13 +0530

    test: custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,4 +25,8 @@ RSpec.describe StringCalculator do
     expect(calc.add("1\n2,3")).to eq(6)
     expect(calc.add("4\n5\n6")).to eq(15)
   end
+
+  it 'supports custom delimiter specified like //;\n1;2' do
+    expect(calc.add("//;\n1;2")).to eq(3)
+  end
 end
